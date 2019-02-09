b259cbb3076c95d1766d452d71654be04da3320c
Simon Barton <simon5@pdx.edu>
2019-02-08 20:17:13 -0800

    Minor improvements
    Got a little done today, but still not functional.

--- gameObjects.js
+++ gameObjects.js
@@ -1,34 +1,35 @@
 //Example on how we could have shared definitions of our objects
 //This is obviously incomplete, but an idea that may help us get started
 
-function spaceship() {
+var spaceship = {
 
-	this.location = [0,0];
-	this.energy = 1000;
-	this.supplies = 100;
-	this.credits = 1000;
-	this.sensor = 0;
-	this.engine = 0;
-	this.damaged = false;
-	this.wormholeRandom = true;
+	location : [0,0],
+	energy : 1000,
+	supplies : 100,
+	credits : 1000,
+	sensor : 0,
+	energyPerDistance : 10,
+	damaged : false,
+	wormholeRandom : false,
 	
-	this.move = function(direction,distance) {	
+	move : function(direction,distance) {	
+		console.log("move was executed");
 	
 	    switch (direction)
 	    {
-	    case "0":
+	    case 0:
 		this.location[0] += distance;
 		break;
 		
-		case "90":
+		case 90:
 		this.location[1] += distance;
 		break;
 		
-		case "180":
+		case 180:
 		this.location[0] -= distance;
 		break;
 		
-		case "270":
+		case 270:
 		this.location[1] -= distance;
 		break;
 		
@@ -37,28 +38,46 @@ function spaceship() {
 		break;
 		}
 		
-		if (inWormhole())
+		this.supplies -= 2;
+		this.energy = this.energy - this.energyPerDistance * distance;
+		
+		//Wormhole check
+		if (this.location[0] < 0 || this.location[0] > 127 || this.location[1] < 0 || this.location[1] > 127)
 		{
 			warmholeWarp();
 		}
-	}
+		
+		setData();
+	},
 	
-	this.inWormhole() {
-		if (this.location[0] < 0 || this.location[0] > 127 || this.location[1] < 0 || this.location[1] > 127)
-			return true;
-		return false;
-	}
 	
-	this.wormholeWarp() {
-	//TODO WRITE FUNCTION
+	wormholeWarp : function() {
+	//TODO write actual random math
+		if (this.wormholeRandom) 
+		{
+			this.location[0] = 75;
+			this.location[1] = 75;
+		}
+		else
+		{
+			this.location[0] = 75;
+			this.location[0] = 75;
+		}	
 	}
 		
 }
 
-//Map would contain 128x128 celestialPoint() objects
-function gameSpace() {
+function setData() {
+	//document.getElementById("location").innerHTML = "(" + spaceship.location[0].toString() + "," + spaceship.location[1].toString() + ")";
+	//document.getElementById("energy").innerHTML = spaceship.energy;
+	//document.getElementById("supplies").innerHTML = spaceship.supplies;
+	//document.getELementById("credits").innerHTML = spaceship.credits;
+}
 
-	this.map = [128][128]
+//Map could contain 128x128 celestialPoint() objects
+var gameSpace = {
+
+	map : [128][128]
 	
 }
 
