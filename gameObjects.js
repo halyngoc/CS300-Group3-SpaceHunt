45eaef0ea2445ffc8aa831fc9fcfc4652e007090
Simon Barton <simon5@pdx.edu>
2019-02-09 19:04:50 -0800

    Finished first implementation of movement
    Fixed indentation in HTML and js files. Got movement to a working point. System status is updated upon movement and spaceship location is set to (75,75) upon wormhole (out of bounds) - this is the non-random wormhole behavior.

--- gameObjects.js
+++ gameObjects.js
@@ -3,88 +3,90 @@
 
 var spaceship = {
 
-	location : [0,0],
-	energy : 1000,
-	supplies : 100,
-	credits : 1000,
-	sensor : 0,
-	energyPerDistance : 10,
-	damaged : false,
-	wormholeRandom : false,
+  location : [0,0],
+  energy : 1000,
+  supplies : 100,
+  credits : 1000,
+  sensor : 0,
+  energyPerDistance : 10,
+  damaged : false,
+  wormholeRandom : false,
+		
+  move : function() {	
+    console.log("newTurn.direction.value is " + newTurn.direction.value);
+    console.log("newTurn.distance.value is " + newTurn.distance.value);
+
+    intDistance = parseInt(newTurn.distance.value);
 	
-	move : function(direction,distance) {	
-		console.log("move was executed");
-	
-	    switch (direction)
-	    {
-	    case 0:
-		this.location[0] += distance;
-		break;
+    console.log("at the start of move x coord is " + this.location[0] + " and y coord is " + this.location[1]);
+
+    switch (newTurn.direction.value)
+    {
+    case "0":
+    this.location[0] += intDistance;
+    break;
 		
-		case 90:
-		this.location[1] += distance;
-		break;
+    case "90":
+    this.location[1] += intDistance;
+    break;
+
+    case "180":
+    this.location[0] -= intDistance;
+    break;
+
+    case "270":
+    this.location[1] -= intDistance;
+    break;
+
+    default:
+    console.log("Invalid direction.");
+    break;
+    }
 		
-		case 180:
-		this.location[0] -= distance;
-		break;
+    this.supplies -= 2;
+    this.energy = this.energy - this.energyPerDistance * intDistance;
+
+    //Wormhole check
+    if (this.location[0] < 0 || this.location[0] > 127 || this.location[1] < 0 || this.location[1] > 127)
+    {
+      alert("You've entered a wormhole!");
+      //Random wormhole behavior is between 1 and 100
+      if (this.wormholeRandom) 
+      {
+        this.location[0] = Math.floor((Math.random() * 100) + 1);
+        this.location[1] = Math.floor((Math.random() * 100) + 1);
+      }
+      else
+      {
+        this.location[0] = 75;
+        this.location[1] = 75;
+      }	
+    }
+
+    setData();
 		
-		case 270:
-		this.location[1] -= distance;
-		break;
-		
-		default:
-		console.log("Invalid direction.");
-		break;
-		}
-		
-		this.supplies -= 2;
-		this.energy = this.energy - this.energyPerDistance * distance;
-		
-		//Wormhole check
-		if (this.location[0] < 0 || this.location[0] > 127 || this.location[1] < 0 || this.location[1] > 127)
-		{
-			warmholeWarp();
-		}
-		
-		setData();
-	},
-	
-	
-	wormholeWarp : function() {
-	//TODO write actual random math
-		if (this.wormholeRandom) 
-		{
-			this.location[0] = 75;
-			this.location[1] = 75;
-		}
-		else
-		{
-			this.location[0] = 75;
-			this.location[0] = 75;
-		}	
-	}
-		
+    console.log("at the END of move x coord is " + this.location[0] + " and y coord is " + this.location[1]);
+    return false;
+  }
+
 }
 
 function setData() {
-	//document.getElementById("location").innerHTML = "(" + spaceship.location[0].toString() + "," + spaceship.location[1].toString() + ")";
-	//document.getElementById("energy").innerHTML = spaceship.energy;
-	//document.getElementById("supplies").innerHTML = spaceship.supplies;
-	//document.getELementById("credits").innerHTML = spaceship.credits;
+  currentStatus.location.value = "(" + spaceship.location[0] + "," + spaceship.location[1] + ")";
+  currentStatus.energy.value = spaceship.energy;
+  currentStatus.supplies.value = spaceship.supplies + "%";
+  currentStatus.credits.value = spaceship.credits;
 }
 
 //Map could contain 128x128 celestialPoint() objects
 var gameSpace = {
-
-	map : [128][128]
-	
+  map : [128][128]
 }
 
 function celestialPoint() {
 
-	this.planet = false;
-	this.asteroid = false;
+  this.planet = false;
+  this.asteroid = false;
 	
-	//etc
+	//Incomplete object constructor for celestial points
 }
