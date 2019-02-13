dde87066af0e43dd197824466d54adbff398e366
QiushiTeng <47011104+QiushiTeng@users.noreply.github.com>
2019-02-12 21:35:23 -0800

    Create Collisions.js
    #5 Collisions

---
+++ Collisions.js
@@ -0,0 +1,16 @@
+<script>
+
+// US-5 Collisions
+// location: [0, 0], using array of 2 elements, location[0]—x, location[1]—y
+
+// x for x axis, location[0]
+// y for y axis, location[1]
+function Collisions(int x, int y) {
+  if(x == ? && y == ?)
+    alert(“Collision, -detail info-\n”);
+  else if ( … )
+    alert(“Collision, …\n”);
+  // …
+}
+
+</script>
