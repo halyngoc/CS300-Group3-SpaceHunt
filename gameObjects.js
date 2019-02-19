4e2b4ca467b40e47cd151e627057c8bca55d1cb2
nvenetucci <47019781+nvenetucci@users.noreply.github.com>
2019-02-18 16:13:24 -0800

    updated and integrated for master merge

--- gameObjects.js
+++ gameObjects.js
@@ -1,17 +1,48 @@
 //Example on how we could have shared definitions of our objects
 //This is obviously incomplete, but an idea that may help us get started
 
+if('Config' in localStorage)
+{
+  var Config = localStorage.getItem('Config');
+  localStorage.removeItem('Config');
+  
+  Config = Config.split('#');
+  var PlayStyle = Config[10];
+
+  localStorage.setItem('PlayType', PlayStyle);  //save play style choice for supply and energy checks
+
+  for(var i = 0; i < Config.length; i++)
+  {
+    Config[i] = parseInt(Config[i], 10);
+  }
+  //window.alert(Config.join("\n"));
+
+  if(Config[7] == 1) {
+    var Rand = true;
+  }else {
+    var Rand = false;
+  }
+
+  var devConfig = true;
+}
+
+//values wont update with dev config until at least one move have been made
+
 var spaceship = {
 
-  location : [0,0],
-  energy : 1000,
-  supplies : 100,
-  credits : 1000,
+  //use the ternary operator for choosing values since if statements cant be used 
+  
+  location : [devConfig ? Config[2] : 0, devConfig ? Config[3] : 0],         //1 //will need to split the 2 values for location
+  energy : devConfig ? Config[4] : 1000,        
+  supplies : devConfig ? Config[5] : 100,       
+  credits : devConfig ? Config[6] : 1000,       
   sensor : 0,
   energyPerDistance : 10,
   damaged : false,
-  wormholeRandom : false,
-  maxCoord : 127,
+  wormholeRandom : devConfig ? Rand : true,   //if dev config has been made this can be set to T or F otherwise it will default to random behavoir 
+  maxCoordX : devConfig ? Config[0] : 127,           
+  maxCoordY : devConfig ? Config[1] : 127,
+
 
   move : function() {	
 
@@ -31,50 +62,52 @@ var spaceship = {
 
 }
 
-function directionCheck() {
-  intDistance = parseInt(document.getElementById("distance").value);
+  function directionCheck() {
 
-  switch (document.getElementById("direction").value) 
-  {
-  case "0":
-  spaceship.location[0] += intDistance;
-  break;
-		
-  case "90":
-  spaceship.location[1] += intDistance;
-  break;
+    intDistance = parseInt(document.getElementById("distance").value);
 
-  case "180":
-  spaceship.location[0] -= intDistance;
-  break;
-
-  case "270":
-  spaceship.location[1] -= intDistance;
-  break;
-
-  default:
-  console.log("Invalid direction.");
-  break;
-  }
-}
-
-function wormholeCheck() {
-  if (spaceship.location[0] < 0 || spaceship.location[0] > spaceship.maxCoord || spaceship.location[1] < 0 || spaceship.location[1] > spaceship.maxCoord)
-  {
-    alert("You've entered a wormhole!");
-    //Random wormhole behavior is between 1 and 100
-    if (this.wormholeRandom) 
+    switch (document.getElementById("direction").value) 
     {
-      spaceship.location[0] = Math.floor((Math.random() * 100) + 1);
-      spaceship.location[1] = Math.floor((Math.random() * 100) + 1);
-    }
-    else
-    {
-      spaceship.location[0] = 75;
-      spaceship.location[1] = 75;
+    case "0":
+    spaceship.location[0] += intDistance;
+    break;
+    
+    case "90":
+    spaceship.location[1] += intDistance;
+    break;
+
+    case "180":
+    spaceship.location[0] -= intDistance;
+    break;
+
+    case "270":
+    spaceship.location[1] -= intDistance;
+    break;
+
+    default:
+    console.log("Invalid direction.");
+    break;
     }
   }
-}
+
+  function wormholeCheck() {
+ //Wormhole check
+    if (spaceship.location[0] < 0 || spaceship.location[0] > spaceship.maxCoordX || spaceship.location[1] < 0 || spaceship.location[1] > spaceship.maxCoordY)
+    {
+      alert("You've entered a wormhole!");
+      //Random wormhole behavior is between 1 and 100, //set to either the base set map size or the dev config map size
+      if (spaceship.wormholeRandom == true) 
+      {
+        spaceship.location[0] = Math.floor((Math.random() * spaceship.maxCoordX) + 1);
+        spaceship.location[1] = Math.floor((Math.random() * spaceship.maxCoordY) + 1);
+      }
+      else
+      {
+        spaceship.location[0] = Config[8];
+        spaceship.location[1] = Config[9];
+      } 
+    }
+  }
 
 function setData() {
   document.getElementById("location").innerHTML = "Current Location: (" + spaceship.location[0] + ", " + spaceship.location[1] + ")";
@@ -84,9 +117,7 @@ function setData() {
 }
 
 //Map could contain 128x128 celestialPoint() objects
-var gameSpace = {
-  map : [spaceship.maxCoord + 1][spaceship.maxCoord + 1]
-};
+var gameSpace = { map : [spaceship.maxCoordX + 1][spaceship.maxCoordY + 1] };
 
 function celestialPoint() {
 
