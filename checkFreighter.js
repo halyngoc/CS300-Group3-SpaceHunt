f1432eba615f96d85de0a125db1553ad2d43fef8
QiushiTeng <47011104+QiushiTeng@users.noreply.github.com>
2019-02-23 22:14:19 -0800

    Add semicolon
    ∙ semicolon added

--- checkFreighter.js
+++ checkFreighter.js
@@ -1,7 +1,7 @@
 function checkFreighter(x, y){
 
   if(gameSpace[x][y].celestialObjects == "freighter"){
-    alert("Freighter found! Energy 1000 & Supplies increased by 2%")
+    alert("Freighter found! Energy 1000 & Supplies increased by 2%");
     return true;
   }
   else {
