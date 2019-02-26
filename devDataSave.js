ae4aa750a7a67c72a11c156714fde94aaeb70650
Liam Rotchford <liamrotchford@gmail.com>
2019-02-25 21:06:56 -0800

    Implemented bounds checking on Map Items
    Bounds checking to the map size for the (Planets, Space Stations, Abandoned Freighter, Meteor Storms, or Asteroids) with automatic value fixing
    
    Changed the value key of values that should be ignored variable upload from -1 to 0.5 since there was bounds checking issues arising for both 0 and -1.

--- devDataSave.js
+++ devDataSave.js
@@ -1,7 +1,7 @@
 function setStateData()
 {
     var RandWrm;
-    var FixWrmLocation = ['0', '0'];
+    var FixWrmLocation = ['0.5', '0.5'];
     var PlayStyleMor;
 
     //textbox value pulls
@@ -37,8 +37,8 @@ function setStateData()
     }
     if(Number(StartLocation[0]) > Number(MapSize[0]) || Number(StartLocation[1]) > Number(MapSize[1]) || Number(StartLocation[0]) < 0 || Number(StartLocation[1]) < 0) {
       window.alert("Current location must be within the range of the map. Setting current location to a point within range.");
-      StartLocation[0] = Number(MapSize[0]) - 1; 
-      StartLocation[1] = Number(MapSize[1]) - 1;
+      StartLocation[0] = Math.floor((Math.random() * MapSize[0] + 1)); 
+      StartLocation[1] = Math.floor((Math.random() * MapSize[1] + 1));
     }
     if(Number(MapSize[0]) < Number(FixWrmLocation[0]) || Number(MapSize[1]) < Number(FixWrmLocation[1]) || Number(FixWrmLocation[0]) < 0 || Number(FixWrmLocation[1]) < 0) {
       window.alert("Fixed wormhold location must be within the range of the map. Setting Fixed wormhole location to Current Location values");
@@ -53,23 +53,25 @@ function setStateData()
     setMapData(StateConfig, MapSize, parser);
 }
 
+
+
 function setMapData(StateConfig, MapSize, parser) {
 
     var i;
-    var PlanetCeleron = ['0', '0']; PlanetXeon = ['0', '0']; PlanetRyzen = ['0', '0'];
+    var PlanetCeleron = ['0.5', '0.5']; PlanetXeon = ['0.5', '0.5']; PlanetRyzen = ['0.5', '0.5'];
     var Planets = [ PlanetCeleron[0], PlanetCeleron[1], PlanetXeon[0], PlanetXeon[1], PlanetRyzen[0], PlanetRyzen[1] ];
 
     var NumberofStations = document.getElementById("StationNum").value;
-    var SpaceStations = ['-1', '-1', '-1', '-1', '-1', '-1'];
+    var SpaceStations = ['0.5', '0.5', '0.5', '0.5', '0.5', '0.5'];
 
     var NumberofFreighters = document.getElementById("FreighterNum").value;
-    var AbandonedFreighter = ['-1', '-1', '-1', '-1', '-1', '-1'];
+    var AbandonedFreighter = ['0.5', '0.5', '0.5', '0.5', '0.5', '0.5'];
 
     var NumberofMeteors = document.getElementById("MeteorNum").value;
-    var MeteorStorms = ['-1', '-1', '-1', '-1', '-1', '-1'];
+    var MeteorStorms = ['0.5', '0.5', '0.5', '0.5', '0.5', '0.5'];
 
     var NumberofAsteroids = document.getElementById("AsteroidsNum").value;
-    var Asteroids = ['-1', '-1', '-1', '-1', '-1', '-1'];
+    var Asteroids = ['0.5', '0.5', '0.5', '0.5', '0.5', '0.5'];
 
 
     if(document.getElementById("RandomPlanet").checked){
@@ -85,10 +87,10 @@ function setMapData(StateConfig, MapSize, parser) {
       PlanetCeleron = PlanetCeleron.split(',');
       PlanetXeon = PlanetXeon.split(',');
       PlanetRyzen = PlanetRyzen.split(',');
+      
+      Planets = [ PlanetCeleron[0], PlanetCeleron[1], PlanetXeon[0], PlanetXeon[1], PlanetRyzen[0], PlanetRyzen[1] ];
     }
     
-    //Planets added to overall string
-    StateConfig = StateConfig.concat(parser, Planets[0], parser, Planets[1], parser, Planets[2], parser, Planets[3], parser, Planets[4], parser, Planets[5]);
     
     //Space stations
     if(NumberofStations != 0) {
@@ -102,14 +104,12 @@ function setMapData(StateConfig, MapSize, parser) {
         SpaceStations = SpaceStations.replace(/ /g, "");
         SpaceStations = SpaceStations.replace(/#/g, ",");
         SpaceStations = SpaceStations.split(',');
-        if(NumberofStations == 1) { SpaceStations[2] = '-1'; SpaceStations[3] = '-1'; SpaceStations[4] = '-1'; SpaceStations[5] = '-1';}
-        else if(NumberofStations == 2) { SpaceStations[4] = '-1'; SpaceStations[5] = '-1'; }
+        if(NumberofStations == 1) { SpaceStations[2] = '0.5'; SpaceStations[3] = '0.5'; SpaceStations[4] = '0.5'; SpaceStations[5] = '0.5';}
+        else if(NumberofStations == 2) { SpaceStations[4] = '0.5'; SpaceStations[5] = '0.5'; }
 
       }      
     }
-    //Space stations added
-    StateConfig = StateConfig.concat(parser, SpaceStations[0], parser, SpaceStations[1], parser, SpaceStations[2], parser, SpaceStations[3], parser, SpaceStations[4], parser, SpaceStations[5]);
-
+   
 
     //Abandoned Freighters 
     if(NumberofFreighters != 0) {
@@ -123,14 +123,12 @@ function setMapData(StateConfig, MapSize, parser) {
         AbandonedFreighter = AbandonedFreighter.replace(/ /g, "");
         AbandonedFreighter = AbandonedFreighter.replace(/#/g, ",");
         AbandonedFreighter = AbandonedFreighter.split(',');
-        if(NumberofFreighters == 1) { AbandonedFreighter[2] = '-1'; AbandonedFreighter[3] = '-1'; AbandonedFreighter[4] = '-1'; AbandonedFreighter[5] = '-1';}
-        else if(NumberofFreighters == 2) { AbandonedFreighter[4] = '-1'; AbandonedFreighter[5] = '-1'; }
+        if(NumberofFreighters == 1) { AbandonedFreighter[2] = '0.5'; AbandonedFreighter[3] = '0.5'; AbandonedFreighter[4] = '0.5'; AbandonedFreighter[5] = '0.5';}
+        else if(NumberofFreighters == 2) { AbandonedFreighter[4] = '0.5'; AbandonedFreighter[5] = '0.5'; }
 
       }      
     }
-    //Abandoned Freighters added
-    StateConfig = StateConfig.concat(parser, AbandonedFreighter[0], parser, AbandonedFreighter[1], parser, AbandonedFreighter[2], parser, AbandonedFreighter[3], parser, AbandonedFreighter[4], parser, AbandonedFreighter[5]);
-
+    
 
     //Meteor Storms
     if(NumberofMeteors != 0) {
@@ -144,14 +142,12 @@ function setMapData(StateConfig, MapSize, parser) {
         MeteorStorms = MeteorStorms.replace(/ /g, "");
         MeteorStorms = MeteorStorms.replace(/#/g, ",");
         MeteorStorms = MeteorStorms.split(',');
-        if(NumberofMeteors == 1) { MeteorStorms[2] = '-1'; MeteorStorms[3] = '-1'; MeteorStorms[4] = '-1'; MeteorStorms[5] = '-1';}
-        else if(NumberofMeteors == 2) { MeteorStorms[4] = '-1'; MeteorStorms[5] = '-1'; }
+        if(NumberofMeteors == 1) { MeteorStorms[2] = '0.5'; MeteorStorms[3] = '0.5'; MeteorStorms[4] = '0.5'; MeteorStorms[5] = '0.5';}
+        else if(NumberofMeteors == 2) { MeteorStorms[4] = '0.5'; MeteorStorms[5] = '0.5'; }
 
       }      
     }
-    //Meteors Storms added
-    StateConfig = StateConfig.concat(parser, MeteorStorms[0], parser, MeteorStorms[1], parser, MeteorStorms[2], parser, MeteorStorms[3], parser, MeteorStorms[4], parser, MeteorStorms[5]);
-
+   
 
     //Asteroids
     if(NumberofAsteroids != 0) {
@@ -165,16 +161,62 @@ function setMapData(StateConfig, MapSize, parser) {
         Asteroids = Asteroids.replace(/ /g, "");
         Asteroids = Asteroids.replace(/#/g, ",");
         Asteroids = Asteroids.split(',');
-        if(NumberofAsteroids == 1) { Asteroids[2] = '-1'; Asteroids[3] = '-1'; Asteroids[4] = '-1'; Asteroids[5] = '-1';}
-        else if(NumberofAsteroids == 2) { Asteroids[4] = '-1'; Asteroids[5] = '-1'; }
+        if(NumberofAsteroids == 1) { Asteroids[2] = '0.5'; Asteroids[3] = '0.5'; Asteroids[4] = '0.5'; Asteroids[5] = '0.5';}
+        else if(NumberofAsteroids == 2) { Asteroids[4] = '0.5'; Asteroids[5] = '0.5'; }
 
       }      
     }
-    //Asteroids added
-    StateConfig = StateConfig.concat(parser, Asteroids[0], parser, Asteroids[1], parser, Asteroids[2], parser, Asteroids[3], parser, Asteroids[4], parser, Asteroids[5]);
+    
 
-    //window.alert(StateConfig);
+    //all 30 Map Items loaded (Planets, Space Stations, Abandoned Freighter, Meteor Storms, or Asteroids)
+    var MapItems = [Planets[0], Planets[1], Planets[2], Planets[3], Planets[4], Planets[5], SpaceStations[0], SpaceStations[1], SpaceStations[2], SpaceStations[3], SpaceStations[4], SpaceStations[5], AbandonedFreighter[0], AbandonedFreighter[1], AbandonedFreighter[2], AbandonedFreighter[3], AbandonedFreighter[4], AbandonedFreighter[5], MeteorStorms[0], MeteorStorms[1], MeteorStorms[2], MeteorStorms[3], MeteorStorms[4], MeteorStorms[5], Asteroids[0], Asteroids[1], Asteroids[2], Asteroids[3], Asteroids[4], Asteroids[5] ];
+    //window.alert(MapItems.join("\n"));
 
-    localStorage.setItem('Config', StateConfig);
+    BoundsChecking(StateConfig, MapItems, MapSize, parser);
+}
 
+
+
+function BoundsChecking(StateConfig, MapItems, MapSize, parser) {
+
+  var i;
+  var ErrorFound = false;
+
+
+  for(i = 0; i < MapItems.length; i+=2) {
+
+      if(Number(MapItems[i]) > Number(MapSize[0]) || Number(MapItems[i]) < 0) {
+        MapItems[i] = Math.floor((Math.random() * MapSize[0] + 1));
+        ErrorFound = true;
+      }
+      
+      if(Number(MapItems[i+1]) > Number(MapSize[1]) || Number(MapItems[i+1]) < 0) {
+        MapItems[i+1] = Math.floor((Math.random() * MapSize[1] + 1));
+        ErrorFound = true;
+      }
+  }
+
+  if(ErrorFound == true) {
+    window.alert("An out of bounds error was found in your specified Map items (Planets, Space Stations, Abandoned Freighter, Meteor Storms, or Asteroids). The values have been corrected to be wintin the bounds of the Map.")
+  }
+  
+
+  //Planets added to overall string
+  StateConfig = StateConfig.concat(parser, MapItems[0], parser, MapItems[1], parser, MapItems[2], parser, MapItems[3], parser, MapItems[4], parser, MapItems[5]);
+  
+  //Space stations added
+  StateConfig = StateConfig.concat(parser, MapItems[6], parser, MapItems[7], parser, MapItems[8], parser, MapItems[9], parser, MapItems[10], parser, MapItems[11]);
+  
+  //Abandoned Freighters added
+  StateConfig = StateConfig.concat(parser, MapItems[12], parser, MapItems[13], parser, MapItems[14], parser, MapItems[15], parser, MapItems[16], parser, MapItems[17]);
+  
+  //Meteors Storms added
+  StateConfig = StateConfig.concat(parser, MapItems[18], parser, MapItems[19], parser, MapItems[20], parser, MapItems[21], parser, MapItems[22], parser, MapItems[23]);
+  
+  //Asteroids added
+  StateConfig = StateConfig.concat(parser, MapItems[24], parser, MapItems[25], parser, MapItems[26], parser, MapItems[27], parser, MapItems[28], parser, MapItems[29]);
+  
+  
+  //window.alert(StateConfig);
+  localStorage.setItem('Config', StateConfig);
 }
