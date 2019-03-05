2010f70d6f36c8c31111894f8e71587c1f34e182
Ha Ly <lyha@pdx.edu>
2019-03-05 12:30:55 -0800

    Refactor
    Semicolon, default value in prompt, and handle case where user clicks cancel or enter invalid input

--- gamble.js
+++ gamble.js
@@ -25,7 +25,7 @@ function handleSpaceStationEvent() {
 }
 
 document.getElementById("gambleBtn").onclick = function() {
-  var amountTakenByUser = parseInt(window.prompt("There are 300 credits in front of you. You are free to take as much as you like, but so is the Casinian. If you take more credits than the Casinian, he will get angry and steal double the amount you've taken. Else, you keep what you've taken. How much will you take?"));
+  var amountTakenByUser = parseInt(window.prompt("There are 300 credits in front of you. You are free to take as much as you like, but so is the Casinian. If you take more credits than the Casinian, he will get angry and steal double the amount you've taken. Else, you keep what you've taken. How much will you take?", 0));
   var amountTakenByCasinian = Math.ceil(Math.random() * 300); // Get a random number between 1 and 300
 
   if (amountTakenByCasinian < amountTakenByUser) {
@@ -35,10 +35,10 @@ document.getElementById("gambleBtn").onclick = function() {
     } else {
       spaceship.credits -= amountTakenByUser;
     }
-  } else if (amountTakenByUser == 0) {
+  } else if (amountTakenByUser == 0 || isNaN(amountTakenByUser)) {
     window.alert("You refused to take anything and leave.");
   } else {
-    window.alert("The Casinian took " + amountTakenByCasinian + " credit(s). You gain " + amountTakenByUser + " credit(s).")
+    window.alert("The Casinian took " + amountTakenByCasinian + " credit(s). You gain " + amountTakenByUser + " credit(s).");
     spaceship.credits += amountTakenByUser;
   }
 
