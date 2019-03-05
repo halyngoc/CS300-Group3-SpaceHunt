8e46bd59fde4fd6c6e5c9b7fe1b0547c274cb95e
Ha Ly <lyha@pdx.edu>
2019-03-05 11:42:02 -0800

    Implement the Casinian game
    Using window.alert for now but I do want to make my own prompt box later on.

--- gamble.js
+++ gamble.js
@@ -11,11 +11,24 @@ function handleSpaceStationEvent() {
 }
 
 document.getElementById("gambleBtn").onclick = function() {
-  if (Math.random() <= 0.5) {
-    // TODO: Gamble is successful. Gain credits
+  var amountTakenByUser = parseInt(window.prompt("There are 300 credits in front of you. You are free to take as much as you like, but so is the Casinian. If you take more credits than the Casinian, he will get angry and steal double the amount you've taken. Else, you keep what you've taken. How much will you take?"));
+  var amountTakenByCasinian = Math.ceil(Math.random() * 300); // Get a random number between 1 and 300
+
+  if (amountTakenByCasinian < amountTakenByUser) {
+    window.alert("The Casinian only took " + amountTakenByCasinian + " credit(s). He is very angry. You lost " + amountTakenByUser + " credit(s).");
+    if (spaceship.credits < amountTakenByUser) {
+      spaceship.credits = 0;
+    } else {
+      spaceship.credits -= amountTakenByUser;
+    }
+  } else if (amountTakenByUser == 0) {
+    window.alert("You refused to take anything and leave.");
   } else {
-    // TODO: Gamble is unsuccessful. Lose credits
+    window.alert("The Casinian took " + amountTakenByCasinian + " credit(s). You gain " + amountTakenByUser + " credit(s).")
+    spaceship.credits += amountTakenByUser;
   }
+
+  setData();
 
   // Hides self cause I'm assuming you only gamble once
   document.getElementById("gambleBtn").classList.add("hidden");
