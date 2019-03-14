5d53db5e9ee57a1976752d63f5db745b5f5d4e5f
Ha Ly <lyha@pdx.edu>
2019-03-14 01:49:52 -0700

    Add music when dying
    Alerts are annoying. They block everything on the page so you can't play music after an alert shows up.
    
    Now I finally understand why no browser game uses alerts

--- death.js
+++ death.js
@@ -18,30 +18,38 @@ else {
 function death(causeOfDeath)
 {
   if(Mortal == true) {
-    if(causeOfDeath == "ENERGYANDSUPPLIES")
-    {
-      if(!alert("YOU RAN OUT OF ENERGY AND SUPPLIES!\nTHE ENTIRE CREW DIES.\n" + "GAME OVER")){window.location.reload();}
-    }
-    else if(causeOfDeath == "ENERGY")
-    {
-      if(!alert("YOU RAN OUT OF ENERGY! THE ENTIRE CREW DIES.\n" + "GAME OVER")){window.location.reload();}
-    }
-    else if(causeOfDeath == "SUPPLIES")
-    {
-      if(!alert("YOU RAN OUT OF SUPPLIES! THE ENTIRE CREW DIES.\n" + "GAME OVER")){window.location.reload();}
-    }
-    else if(causeOfDeath == "ASTEROID")
-    {
-      if(!alert("YOU COLLIDE WITH AN ASTEROID! THE SHIP EXPLODES.\n" + "GAME OVER")){window.location.reload();}
-    }
-    else if(causeOfDeath == "BADMAX")
-    {
-      if(!alert("BAD MAX HAS ATTACKED AND DESTROYED YOUR SHIP!\n" + "EVERYONE HAS DIED.\n" + "GAME OVER")){window.location.reload();}
-    }
-    else
-    {
-      if(!alert(causeOfDeath + "! CELESTIAL BODY COLLISION!\nTHE SHIP EXPLODES.\n" + "GAME OVER")){window.location.reload();}
-    }
+    playMusic("die");
+    setTimeout(function() {
+      alertDeath(causeOfDeath);
+    }, 2000);
+
     return true;
+  }
+}
+
+function alertDeath(causeOfDeath) {
+  if(causeOfDeath == "ENERGYANDSUPPLIES")
+  {
+    if(!alert("YOU RAN OUT OF ENERGY AND SUPPLIES!\nTHE ENTIRE CREW DIES.\n" + "GAME  OVER")){window.location.reload();}
+  }
+  else if(causeOfDeath == "ENERGY")
+  {
+    if(!alert("YOU RAN OUT OF ENERGY! THE ENTIRE CREW DIES.\n" + "GAME OVER"))  {window.location.reload();}
+  }
+  else if(causeOfDeath == "SUPPLIES")
+  {
+    if(!alert("YOU RAN OUT OF SUPPLIES! THE ENTIRE CREW DIES.\n" + "GAME OVER"))  {window.location.reload();}
+  }
+  else if(causeOfDeath == "ASTEROID")
+  {
+    if(!alert("YOU COLLIDE WITH AN ASTEROID! THE SHIP EXPLODES.\n" + "GAME OVER"))  {window.location.reload();}
+  }
+  else if(causeOfDeath == "BADMAX")
+  {
+    if(!alert("BAD MAX HAS ATTACKED AND DESTROYED YOUR SHIP!\n" + "EVERYONE HAS DIED.\n" + "GAME  OVER")){window.location.reload();}
+  }
+  else
+  {
+    if(!alert(causeOfDeath + "! CELESTIAL BODY COLLISION!\nTHE SHIP EXPLODES.\n" + "GAME OVER") ){window.location.reload();}
   }
 }
