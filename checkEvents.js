eb447d1aee17decf92514ecddd87fca7edecb668
Ha Ly <lyha@pdx.edu>
2019-03-14 02:22:09 -0700

    Add music on encountering Bad Max

--- checkEvents.js
+++ checkEvents.js
@@ -70,24 +70,28 @@ function checkEvents(x, y){
   
     //Bad Max is hard coded to [7][7] for now
   if(gameSpace[x][y].celestialObjects.includes("Bad Max")){
-    randomNum = Math.floor(Math.random() * 100);
-    console.log("randomNum is " + randomNum);
+    playMusic("bad max");
+    setTimeout(function() {
+      randomNum = Math.floor(Math.random() * 100);
+      console.log("randomNum is " + randomNum);
+      
+      //We fight off Bad Max and live
+      if (randomNum < 33){
+        alert("You've encountered Bad Max... Fortunately, your crew was able to heroically fight them off successfully!");
+      }
+      //Bad Max boards the ship and steals all credits and half of supplies
+      else if (randomNum > 66){
+        alert("Bad Max has boarded your ship! His crew of goons stole all your credits and half of your supplies!");
+        spaceship.credits = 0;
+        spaceship.supplies = Math.floor(spaceship.supplies / 2); 
+      }
+      //Bad Max blows up your ship, everyone dies, game over
+      else{
+        alert("BAD MAX HAS ATTACKED AND DESTROYED YOUR SHIP! EVERYONE HAS DIED. GAME OVER.");
+        window.location.reload();
+      }
+    }, 1000);
     
-    //We fight off Bad Max and live
-    if (randomNum < 33){
-      alert("You've encountered Bad Max... Fortunately, your crew was able to heroically fight them off successfully!");
-    }
-    //Bad Max boards the ship and steals all credits and half of supplies
-    else if (randomNum > 66){
-      alert("Bad Max has boarded your ship! His crew of goons stole all your credits and half of your supplies!");
-      spaceship.credits = 0;
-      spaceship.supplies = Math.floor(spaceship.supplies / 2); 
-    }
-    //Bad Max blows up your ship, everyone dies, game over
-    else{
-      alert("BAD MAX HAS ATTACKED AND DESTROYED YOUR SHIP! EVERYONE HAS DIED. GAME OVER.");
-      window.location.reload();
-    }
     return 0;
   }
 
